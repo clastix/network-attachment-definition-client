ba607fd022f7a171359db6de898e8b1bc6551db7
Dario Tranchitella <dario@tranchitella.eu>
2026-06-16 16:06:02 +0200

    feat: add applyconfigurations generation for SSA support
    Signed-off-by: Dario Tranchitella <dario@tranchitella.eu>
    Co-authored-by: Patryk Rostkowski <patryk@clastix.io>
    Signed-off-by: Dario Tranchitella <dario@tranchitella.eu>

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -4,31 +4,22 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-SCRIPT_ROOT=$(dirname ${BASH_SOURCE})/..
+SCRIPT_ROOT=$(dirname "${BASH_SOURCE[0]}")/..
 CODEGEN_PKG=${CODEGEN_PKG:-$(cd "${SCRIPT_ROOT}"; ls -d -1 ./vendor/k8s.io/code-generator 2>/dev/null || echo ../code-generator)}
 
 source "${CODEGEN_PKG}/kube_codegen.sh"
 
-# generate the code with:
-# --output-base    because this script should also be able to run inside the vendor dir of
-#                  k8s.io/kubernetes. The output-base is needed for the generators to output into the vendor dir
-#                  instead of the $GOPATH directly. For normal projects this can be dropped.
+THIS_PKG="github.com/k8snetworkplumbingwg/network-attachment-definition-client"
 
 kube::codegen::gen_helpers \
-    --input-pkg-root github.com/k8snetworkplumbingwg/network-attachment-definition-client/pkg/apis \
-    --output-base "$(dirname "${BASH_SOURCE[0]}")/.." \
-    --boilerplate "${SCRIPT_ROOT}/hack/custom-boilerplate.go.txt"
-
-#kube::codegen::gen_openapi \
-#    --input-pkg-root github.com/k8snetworkplumbingwg/network-attachment-definition-client/pkg/apis \
-#    --output-pkg-root github.com/k8snetworkplumbingwg/network-attachment-definition-client/pkg/apiserver \
-#    --output-base "$(dirname "${BASH_SOURCE[0]}")/.." \
-#    --boilerplate "${SCRIPT_ROOT}/hack/custom-boilerplate.go.txt"
+    --boilerplate "${SCRIPT_ROOT}/hack/custom-boilerplate.go.txt" \
+    "${SCRIPT_ROOT}/pkg/apis"
 
 kube::codegen::gen_client \
     --with-watch \
-    --input-pkg-root github.com/k8snetworkplumbingwg/network-attachment-definition-client/pkg/apis \
-    --output-pkg-root github.com/k8snetworkplumbingwg/network-attachment-definition-client/pkg/clients \
-    --output-base "$(dirname "${BASH_SOURCE[0]}")/.." \
-    --boilerplate "${SCRIPT_ROOT}/hack/custom-boilerplate.go.txt"
+    --with-applyconfig \
+    --output-dir "${SCRIPT_ROOT}/pkg/clients" \
+    --output-pkg "${THIS_PKG}/pkg/clients" \
+    --boilerplate "${SCRIPT_ROOT}/hack/custom-boilerplate.go.txt" \
+    "${SCRIPT_ROOT}/pkg/apis"
 
